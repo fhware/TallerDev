17f2c6dbc5e41af6c64decb36f9e76237776dd5a
Felipe Hernandez DATA <felipe.hernandez@arandasoft.com>
2019-12-20 08:50:55 -0500

    Código Fuente

---
+++ TallerDev/TallerDev/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TallerDev.Startup))]
+namespace TallerDev
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
